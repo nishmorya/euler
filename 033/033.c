249d1b8ec5ba1dc79bd1405759cabf6ac449a78b
Nishani Ratnaweera <nishmorya@gmail.com>
2018-01-26 11:53:58 +0530

    Numerators and denominators are simulated.

--- 033/033.c
+++ 033/033.c
@@ -1,8 +1,15 @@
 #include <stdio.h>
 
+#define START 10
+#define END 99
+
 int main()
 {
-
+	for (int i = START; i < END; i++) {
+		for (int j = i + 1; j < END; j++) {
+			printf("%d %d\n", i, j);
+		}
+	}
 
 	return 0;
 }
