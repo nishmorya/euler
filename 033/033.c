8f754f7cf817428e33109a8c828143b0373c38d0
Nishani Ratnaweera <nishmorya@gmail.com>
2018-01-26 22:49:55 +0530

    Skip if the denominator's digits are the reverse of numerator.

--- 033/033.c
+++ 033/033.c
@@ -6,7 +6,12 @@
 int main()
 {
 	for (int i = START; i < END; i++) {
+		float nu_d1 = i / 10;
+		float nu_d2 = i % 10;
+		float riverse_nu = nu_d2 * 10 + nu_d1;
 		for (int j = i + 1; j < END; j++) {
+			if (riverse_nu == j)
+				continue;
 			printf("%d %d\n", i, j);
 		}
 	}
