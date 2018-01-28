9c259f4e9eabb10e362133add1612154008f75f5
Nishani Ratnaweera <nishmorya@gmail.com>
2018-01-28 19:39:45 +0530

    Ratio between numerator and denominator is calculated.

--- 033/033.c
+++ 033/033.c
@@ -10,9 +10,13 @@ int main()
 		float nu_d2 = i % 10;
 		float riverse_nu = nu_d2 * 10 + nu_d1;
 		for (int j = i + 1; j < END; j++) {
+			float de_d1 = j / 10;
+			float de_d2 = j % 10;
+			float value1 = (nu_d1 * 10 + nu_d2) / (de_d1 * 10 + de_d2);
 			if (riverse_nu == j)
 				continue;
-			printf("%d %d\n", i, j);
+			printf("%d %d ", i, j);
+			printf("%f\n", value1);
 		}
 	}
 
