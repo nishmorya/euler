0347aea5e3827189fa1c428c35f1f1e4660cdb60
Nishani Ratnaweera <nishmorya@gmail.com>
2018-01-29 15:42:49 +0530

    Implementation of factorial function.

--- 034/034.c
+++ 034/034.c
@@ -1,7 +1,19 @@
 #include <stdio.h>
 
+int factorial(int n)
+{
+	int fac = 0;
+	if (n == 1) {
+		return 1;
+	} else {
+		fac += n * factorial(n - 1);
+	}
+	return fac;
+}
+
 int main()
 {
+	printf("%d\n", factorial(3));
 	return 0;
 }
 
