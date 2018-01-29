a80d78f517327116e988c084615330b7b07988ca
Nishani Ratnaweera <nishmorya@gmail.com>
2018-01-29 15:53:14 +0530

    Modifying factorial function.

--- 034/034.c
+++ 034/034.c
@@ -3,7 +3,9 @@
 int factorial(int n)
 {
 	int fac = 0;
-	if (n == 1) {
+	if (n == 0) {
+		return 1;
+	} else if (n == 1) {
 		return 1;
 	} else {
 		fac += n * factorial(n - 1);
@@ -13,7 +15,7 @@ int factorial(int n)
 
 int main()
 {
-	printf("%d\n", factorial(3));
+	printf("%d\n", factorial(0));
 	return 0;
 }
 
