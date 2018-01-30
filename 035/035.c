67fee5c70b719a0d1261b2029dccb84777f05b61
Nishani Ratnaweera <nishmorya@gmail.com>
2018-01-30 14:44:48 +0530

    Implementing is_prime function.

--- 035/035.c
+++ 035/035.c
@@ -1,7 +1,27 @@
 #include <stdio.h>
 
+int is_prime(int n)
+{
+	if (n % 2 == 0)
+		return 0;
+	else if (n % 3 == 0)
+		return 0;
+	else {
+		for (int i = 5; i <= n / 2; i = i + 2) {
+			if (n % i == 0)
+				return 0;
+		}
+		return 1;
+	}
+
+}
+
 int main()
 {
+	for (int i = 1; i <= 50; i++) {
+		printf("%d\n", is_prime(i));
+	}
+
 	return 0;
 }
 
